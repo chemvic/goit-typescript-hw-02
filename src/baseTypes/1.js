9fd55d7240bb7617c81de166fba1ec4f8ffa4f12
chemvic <chemvic79@gmail.com>
2023-10-08 16:19:28 +0300

    5 домашек

---
+++ src/baseTypes/1.js
@@ -0,0 +1,9 @@
+"use strict";
+// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+exports.__esModule = true;
+var age = 50;
+var name = 'Max';
+var toggle = true;
+var empty = null;
+var notInitialize;
+var callback = function (a) { return 100 + a; };
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,19 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum Weekdays{
+  Sunday,
+  Monday, Tuesday,
+  Wednesday, Thursday, Friday, Saturday
+};
+function isWeekend (day: Weekdays): boolean{
+ switch (day) {
+  case Weekdays.Sunday:
+  case Weekdays.Saturday:
+  return true;  
+ 
+  default:  
+  return false;
+}};
+
+isWeekend(Weekdays.Monday);
